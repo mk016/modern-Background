914a371c43dadc2c2620400ddbe98445da7c8a86
Mk016 <mahendrakumawat80224@gmail.com>
2024-08-07 19:22:57 +0530

    a

--- app/page.tsx
+++ app/page.tsx
@@ -88,7 +88,7 @@ export default function Home() {
               </p>
               <div className="mt-10 flex gap-4">
                 <a
-                  href="https://github.com/ibelick/background-snippets"
+                  href="https://github.com/mk016"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center"
